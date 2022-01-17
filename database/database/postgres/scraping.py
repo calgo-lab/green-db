47e2302b83c37f7dbf71c1107aa5ab413ccd5338
Sebastian Jäger <message@sebastian-jaeger.me>
2022-01-17 17:54:53 +0100

    refactor namings. Implement some helper methods.

--- database/database/postgres/scraping.py
+++ database/database/postgres/scraping.py
@@ -1,13 +1,18 @@
+from datetime import datetime
+from logging import getLogger
+from typing import List
+
 from sqlalchemy import Column
 from sqlalchemy.dialects.postgresql import JSONB
 from sqlalchemy.sql.sqltypes import INTEGER, TEXT, TIMESTAMP, VARCHAR
 
-from database.domain import ScrapedPage
 from . import PostgresBaseClass
 from .config import SCRAPING_DB_OTTO_TABLE_NAME, SCRAPING_DB_ZALANDO_TABLE_NAME
 
+logger = getLogger(__name__)
 
-class __Scraping:
+
+class BaseScraping:
     id = Column(INTEGER, nullable=False, autoincrement=True, primary_key=True)
     start_timestamp = Column(TIMESTAMP, nullable=False)
     url = Column(TEXT, nullable=False)
@@ -16,13 +21,37 @@ class __Scraping:
     category = Column(VARCHAR(length=80), nullable=False)
     meta_information = Column(JSONB, nullable=True)
 
+    @classmethod
+    def get_columns(cls) -> List[str]:
+        return [a for a in cls.__dict__.keys() if not a.startswith("_")]
 
-class ScrapingZalando(PostgresBaseClass, __Scraping):
+    def __repr__(self):
+        columns = self.get_columns()
+
+        def get_value_depending_on_type(column: str) -> str:
+            value = getattr(self, column)
+            if type(value) == str or type(value) == datetime:
+                return f"'{value}'"
+            else:
+                return f"{value}"
+
+        column_values_string = [
+            f"{column}={get_value_depending_on_type(column)}"
+            for column in columns
+            if hasattr(self, column)
+        ]
+        return f"{self.__class__.__name__}({', '.join(column_values_string)})"
+
+
+class ZalandoScraping(PostgresBaseClass, BaseScraping):
     __tablename__ = SCRAPING_DB_ZALANDO_TABLE_NAME
 
 
-class ScrapingOTTO(PostgresBaseClass, __Scraping):
+class OTTOScraping(PostgresBaseClass, BaseScraping):
     __tablename__ = SCRAPING_DB_OTTO_TABLE_NAME
 
 
-# TODO Lesen schreiben zugriffe..
+SCRAPING_DB_CLASS_FOR = {
+    SCRAPING_DB_ZALANDO_TABLE_NAME: ZalandoScraping,
+    SCRAPING_DB_OTTO_TABLE_NAME: OTTOScraping,
+}
