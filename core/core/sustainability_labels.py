65bd0bc1cc36ee15417bae9a3aefcc19fd9bf859
Sebastian Jäger <message@sebastian-jaeger.me>
2022-03-09 09:34:05 +0100

    minor changes: pydoc, variable/fucntion names ...

--- core/core/sustainability_labels.py
+++ core/core/sustainability_labels.py
@@ -7,8 +7,7 @@ from typing import Any, Dict, List, Optional, Type
 from pydantic import BaseModel, conint
 
 SUSTAINABILITY_LABELS_DATA_DIR = Path(__file__).parent / "sustainability_labels_data"
-information_from = datetime(2022, 3, 3, 19)
-certificate_tag = "certificate:"
+certificate_prefix = "certificate:"
 
 # Add special labels (not included in JSON) manually
 special_labels = {
@@ -31,13 +30,13 @@ special_labels = {
 }
 
 # Load JSON files with certificate_information and certificate_evaluation
-with open(SUSTAINABILITY_LABELS_DATA_DIR / "sustainability-labels.json") as labels_file:
-    certificate_information = json.load(labels_file)
+with open(SUSTAINABILITY_LABELS_DATA_DIR / "sustainability-labels.json") as certificates_file:
+    certificate_information = json.load(certificates_file)
 
 with open(
     SUSTAINABILITY_LABELS_DATA_DIR / "sustainability_labels_evaluation.json"
-) as evaluation_file:
-    certificate_evaluation = json.load(evaluation_file)
+) as evaluations_file:
+    certificate_evaluation = json.load(evaluations_file)
 
 # Combine the two JSON files
 # by adding the sustainability evaluation information to the corresponding certificate
@@ -48,7 +47,7 @@ certificate_information.update(special_labels)
 
 # Create a mapping from the full certificate name to its id without the certificate_tag
 _certificate_2_id = {
-    certificate.replace(certificate_tag, ""): certificate
+    certificate.replace(certificate_prefix, ""): certificate
     for certificate in certificate_information.keys()
 }
 
@@ -79,16 +78,22 @@ def get_certificate_class() -> Type[Enum]:
 
 
 
-def get_certificate_attribute(
+def _get_certificate_attribute(
     certificate_information_dict: Dict[str, Dict[str, Any]],
     attribute: str,
     language_preference: List[str] = ["de", "en", "fr"],
 ) -> str:
     """
     Helper function to retrieve an attribute of a certificate_information_dict in one language.
-    The language_preference is by default ["de", "en", "fr"].
+    Args:
+        certificate_information_dict (Dict[str, Dict[str, Any]]): Dictionary that holds the
+            sustainability label information.
+        attribute (str): `attribute` to get.
+        language_preference (List[str], optional): Order of language priorities.
+            Defaults to ["de", "en", "fr"].
+
     Returns:
-        str: of the corresponding attribute in one language.
+       str: of the corresponding attribute in one language.
     """
     for language in language_preference:
         if language in certificate_information_dict["languages"].keys():
@@ -100,9 +105,9 @@ def get_certificate_attribute(
 certificate_information_dense = [
     SustainabilityLabel(
         id=certificate_id,
-        timestamp=information_from,
-        name=get_certificate_attribute(certificate_information_dict, "name"),
-        description=get_certificate_attribute(certificate_information_dict, "description"),
+        timestamp=datetime(2022, 3, 3, 19),  # NOTE: Change me after updating sustainability labels
+        name=_get_certificate_attribute(certificate_information_dict, "name"),
+        description=_get_certificate_attribute(certificate_information_dict, "description"),
         ecological_evaluation=certificate_information_dict.get("ecological_evaluation", None),
         social_evaluation=certificate_information_dict.get("social_evaluation", None),
         credibility_evaluation=certificate_information_dict.get("social_evaluation", None),
