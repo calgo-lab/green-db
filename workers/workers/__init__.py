ceddc809d09617b224050a9e875175543aff1b93
BigDatalex <alex.flick@gmx.de>
2022-03-22 13:54:21 +0100

    Create Asos tables.

--- workers/workers/__init__.py
+++ workers/workers/__init__.py
@@ -1,5 +1,6 @@
 from core import log
-from core.constants import TABLE_NAME_SCRAPING_OTTO, TABLE_NAME_SCRAPING_ZALANDO
+from core.constants import TABLE_NAME_SCRAPING_OTTO, TABLE_NAME_SCRAPING_ZALANDO, \
+    TABLE_NAME_SCRAPING_ASOS
 from database.connection import Scraping
 
 log.setup_logger(__name__)
@@ -7,4 +8,5 @@ log.setup_logger(__name__)
 CONNECTION_FOR_TABLE = {
     TABLE_NAME_SCRAPING_ZALANDO: Scraping(TABLE_NAME_SCRAPING_ZALANDO),
     TABLE_NAME_SCRAPING_OTTO: Scraping(TABLE_NAME_SCRAPING_OTTO),
+    TABLE_NAME_SCRAPING_ASOS: Scraping(TABLE_NAME_SCRAPING_ASOS),
 }
