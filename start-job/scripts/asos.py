94659c8b962973f719448c2e65e82c2b1fa43ac6
BigDatalex <alex.flick@gmx.de>
2022-04-04 13:58:22 +0200

    Add directory independant filepath and filter non-specified mappings.

--- start-job/scripts/asos.py
+++ start-job/scripts/asos.py
@@ -1,10 +1,13 @@
 import json
 from typing import Dict, List, Tuple, Union
+from pathlib import Path
 
 gender_to_category = {
     "female": 28981,
     "male": 28982
 }
+
+asos_file_path = Path(__file__).parent.parent / "data/asos_product_types.json"
 
 
 def read_json(path):
@@ -14,7 +17,7 @@ def read_json(path):
 
 def combine_results(gender_mapping: dict,
                     gender: str,
-                    categories_json_path: str = "../data/asos_product_types.json",
+                    categories_json_path: Path,
                     serp_api: str = "https://www.asos.com/api/product/search/v2/categories/",
                     serp_filters: str = "&channel=mobile-web&country=FR&currency=EUR"
                                    "&keyStoreDataversion=dup0qtf-35&lang=fr-FR&limit=72&offset=0"
@@ -25,24 +28,27 @@ def combine_results(gender_mapping: dict,
     for id, info in categories.items():
         if gender in info.get("gender"):
             mapping = info.get("mapping")
-            category, meta_data = mapping if type(mapping) == list else (mapping, {})
-            results.append(
-                {
-                    "start_urls": f"{serp_api}{gender_mapping.get(gender)}"
-                                  f"?attribute_1047={id}{serp_filters}",
-                    "category": category,
-                    "meta_data": json.dumps({"family": "FASHION", "sex": gender, **meta_data}),
-                }
-            )
+            if mapping:  # exclude categories for which we do not have a mapping yet
+                category, meta_data = mapping if type(mapping) == list else (mapping, {})
+                results.append(
+                    {
+                        "start_urls": f"{serp_api}{gender_mapping.get(gender)}"
+                                      f"?attribute_1047={id}{serp_filters}",
+                        "category": category,
+                        "meta_data": json.dumps({"family": "FASHION", "sex": gender, **meta_data}),
+                    }
+                )
     return results
 
 
 def male():
-    return combine_results(gender_mapping=gender_to_category, gender="male")
+    return combine_results(gender_mapping=gender_to_category, gender="male",
+                           categories_json_path=asos_file_path)
 
 
 def female():
-    return combine_results(gender_mapping=gender_to_category, gender="female")
+    return combine_results(gender_mapping=gender_to_category, gender="female",
+                           categories_json_path=asos_file_path)
 
 
 def get_settings() -> List[Union[Dict[str, str], Dict[str, Tuple[str, Dict[str, str]]]]]:
