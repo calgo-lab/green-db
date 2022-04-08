576ab6b9d916b8d0a9bde85bae79d2289cdcaf44
en-GB <67548055+en-GB@users.noreply.github.com>
2022-04-08 15:44:16 +0200

    scraping table

--- database/database/tables.py
+++ database/database/tables.py
@@ -8,6 +8,7 @@ from core.constants import (
     TABLE_NAME_SCRAPING_ASOS,
     TABLE_NAME_SCRAPING_OTTO,
     TABLE_NAME_SCRAPING_ZALANDO,
+    TABLE_NAME_SCRAPING_ZALANDO_FR,
     TABLE_NAME_SUSTAINABILITY_LABELS,
 )
 
@@ -89,6 +90,18 @@ class ZalandoScrapingTable(ScrapingBaseTable, ScrapingTable):
     __tablename__ = TABLE_NAME_SCRAPING_ZALANDO
 
 
+class ZalandoFrScrapingTable(ScrapingBaseTable, ScrapingTable):
+    """
+    The actual scraping table for Zalando.
+
+    Args:
+        ScrapingBaseTable ([type]): `sqlalchemy` base class for the Scraping database
+        ScrapingTable ([type]): To inherit the table definition
+    """
+
+    __tablename__ = TABLE_NAME_SCRAPING_ZALANDO_FR
+
+
 class OTTOScrapingTable(ScrapingBaseTable, ScrapingTable):
     """
     The actual scraping table for Otto.
@@ -115,6 +128,7 @@ class AsosScrapingTable(ScrapingBaseTable, ScrapingTable):
 
 # Used to dynamically map a table name to the correct Table class.
 SCRAPING_TABLE_CLASS_FOR: Dict[str, Type[ScrapingTable]] = {
+    TABLE_NAME_SCRAPING_ZALANDO_FR: ZalandoFrScrapingTable,
     TABLE_NAME_SCRAPING_ZALANDO: ZalandoScrapingTable,
     TABLE_NAME_SCRAPING_OTTO: OTTOScrapingTable,
     TABLE_NAME_SCRAPING_ASOS: AsosScrapingTable,
