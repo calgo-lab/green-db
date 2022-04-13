d56fedb13f2c0f5370b75a601c9a645d1a7413d2
Sebastian Jäger <git@sebastian-jaeger.me>
2022-04-13 09:18:35 +0200

    minor

--- start-job/scripts/entrypoint.sh
+++ start-job/scripts/entrypoint.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
 
+# clone green-db repositore
 cd /
 git clone https://github.com/calgo-lab/green-db.git
+
+# install necessary dependencies
 cd /green-db/core
 poetry build -f wheel && pip install --no-deps dist/*.whl 
 
+# deploy to scrapyd
+cd /green-db/scraping
 scrapyd-client deploy in-cluster
 
-# finally start all scraping jobs
+# and finally start all scraping jobs
 cd /workdir/scripts
 python main.py
