e5d7a1818f032ec2fd7f2f94b3a062f97f6486a6
BigDatalex <alex.flick@gmx.de>
2022-04-20 19:08:50 +0200

    Otto specific parse_page handling.

--- extract/extract/parse.py
+++ extract/extract/parse.py
@@ -24,13 +24,22 @@ def parse_page(scraped_page: ScrapedPage) -> ParsedPage:
     Parses the given `scraped_page` and extract its schema.org information.
 
     Args:
+        unescape:
         scraped_page (ScrapedPage): Domain object `ScrapedPage`
 
     Returns:
         ParsedPage: Representation that bundles the `scraped_page` with intermediate representations
     """
     beautiful_soup = BeautifulSoup(scraped_page.html, "html.parser")
-    schema_org = extract_schema_org(scraped_page.html)
+
+    # otto schema extraction fails sometimes when unescaped, there is no official fix yet
+    # see: https://github.com/scrapinghub/extruct/issues/175
+    if scraped_page.merchant == "otto":
+        unescape = False
+    else:
+        unescape = True
+
+    schema_org = extract_schema_org(scraped_page.html, unescape)
     return ParsedPage(
         scraped_page=scraped_page, beautiful_soup=beautiful_soup, schema_org=schema_org
     )
@@ -46,16 +55,21 @@ MICRODATA = "microdata"
 _SYNTAXES = [DUBLINCORE, JSON_LD, MICRODATA]
 
 
-def extract_schema_org(page_html: str) -> dict:
+def extract_schema_org(page_html: str, unescape: bool) -> dict:
     """
     Extract schema.org information form `page_html`.
 
     Args:
+        unescape (boolean): boolean whether to unescape the html before extraction or not
         page_html (str): HTML of the page
 
     Returns:
         dict: Schema.org information found in `page_html`
     """
-    unescaped_html = html.unescape(page_html)
-    schema_org = extruct.extract(unescaped_html, syntaxes=_SYNTAXES, errors="ignore")
+
+    if unescape:
+        unescaped_html = html.unescape(page_html)
+        schema_org = extruct.extract(unescaped_html, syntaxes=_SYNTAXES)
+    else:
+        schema_org = extruct.extract(page_html, syntaxes=_SYNTAXES)
     return schema_org if schema_org else {}
