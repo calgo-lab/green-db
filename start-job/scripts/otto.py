af1466f0dd09ea5247168ff4318d2c5286236a81
BigDatalex <alex.flick@gmx.de>
2022-04-21 13:33:42 +0200

    Change blazer from SWEATER to SUIT.

--- start-job/scripts/otto.py
+++ start-job/scripts/otto.py
@@ -7,7 +7,7 @@ def female_clothes() -> List[dict]:
     filter = "?nachhaltigkeit=alle-nachhaltigen-artikel"
 
     path_2_category = {
-        "blazer": "SWEATER",
+        "blazer": "SUIT",
         "blusen": "BLOUSE",
         "hosen": "PANT",
         "jacken": "JACKET",
