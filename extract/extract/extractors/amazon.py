04f02bcf74b984977607fbc01fb1a71223784cb3
wkrl <kwxrl@yandex.com>
2022-04-25 21:16:30 +0200

    Add price and size extraction

--- extract/extract/extractors/amazon.py
+++ extract/extract/extractors/amazon.py
@@ -8,7 +8,6 @@ from pydantic import ValidationError
 from core.domain import CertificateType, Product
 from core.sustainability_labels import load_and_get_sustainability_labels
 from ..parse import ParsedPage
-from ..utils import safely_return_first_element
 
 logger = getLogger(__name__)
 
@@ -22,6 +21,10 @@ def sustainability_label_to_certificate(labels) -> list[CertificateType]:
         if any(_get_matchin_languages(description["languages"].values(), labels))
     }
 
+    # TODO: Are there more labels that need manual mapping?
+    if "Global Recycled Standard" in labels:
+        result.update({CertificateType.GLOBAL_RECYCLED_STANDARD})
+
     return result or {CertificateType.OTHER}
 
 
@@ -33,14 +36,12 @@ def _get_matchin_languages(languages, labels):
     ]
 
 
-def find_from_detail_table(soup, prop):
+def find_from_detail_list(soup, prop):
     product_details = soup.find("div", {"id": "detailBulletsWrapper_feature_div"})
     parent = product_details.find("span", text=re.compile(f"^{prop}")).parent
     return parent.findAll("span")[1].text
 
 
-# TODO How to solve range of prices?
-# TODO Is a currency mapping necessary?
 def extract_amazon(parsed_page: ParsedPage) -> Optional[Product]:
     soup = parsed_page.beautiful_soup
 
@@ -48,24 +49,30 @@ def extract_amazon(parsed_page: ParsedPage) -> Optional[Product]:
     color = soup.find(
         "span", {"id": "inline-twister-expanded-dimension-text-color_name"}).text.strip()
 
+    sizes = soup.find_all(
+        "span", {"class", "a-size-base swatch-title-text-display swatch-title-text"})[1:]
+    size = ", ".join([size.text.strip() for size in sizes])
+
     images = soup.find("div", {"id": "altImages"}).find_all("img")
     image_urls = [
         image["src"]
         for image in images
-        if not image["src"].endswith(".gif")  # Filter out transparent gif
+        if not image["src"].endswith(".gif")
+        and "play-button-overlay" not in image["src"]
     ]
+
+    currency = "EUR"
+    price_range = soup.find("span", {"class": "a-price-range"})
+    prices = price_range.find_all("span", {"class": "a-offscreen"})
+    price = float(prices[-1].text[1:])  # Return the maximum price
 
     sustainability_spans = soup.find_all("span", id=re.compile("CPF-BTF-Certificate-Name"))
     sustainability_texts = [span.text for span in sustainability_spans]
     sustainability_labels = sustainability_label_to_certificate(sustainability_texts)
 
-    brand = find_from_detail_table(soup, "Manufacturer")
-    asin = find_from_detail_table(soup, "ASIN")
+    brand = find_from_detail_list(soup, "Manufacturer")
+    asin = find_from_detail_list(soup, "ASIN")
     description = soup.find("div", {"id": "productDescription"}).p.get_text().strip()
-
-    size = None
-    price = 69.80
-    currency = "EUR"
 
     return Product(
         timestamp=parsed_page.scraped_page.timestamp,
