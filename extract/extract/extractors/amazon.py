dac64dfefe62dc4f3887faee6384dec1b27265ba
AdriaSG <adrianasg.15@gmail.com>
2022-04-27 13:02:43 +0200

    Adjustment for testing sustainability labels

--- extract/extract/extractors/amazon.py
+++ extract/extract/extractors/amazon.py
@@ -7,10 +7,57 @@ from pydantic import ValidationError
 
 from core.domain import CertificateType, Product
 from core.sustainability_labels import load_and_get_sustainability_labels
-from ..parse import ParsedPage
+from ..parse import MICRODATA, ParsedPage
 
 logger = getLogger(__name__)
 
+def extract_amazon(parsed_page: ParsedPage) -> Optional[Product]:
+    soup = parsed_page.beautiful_soup
+
+    name = soup.find("span", {"id": "productTitle"}).text.strip()
+    color = soup.find("span", {"id": "inline-twister-expanded-dimension-text-color_name"}).text.strip()
+
+    sizes = soup.find_all("span", {"class", "a-size-base swatch-title-text-display swatch-title-text"})[1:]
+    size = ", ".join([size.text.strip() for size in sizes])
+
+    images = soup.find("div", {"id": "altImages"}).find_all("img")
+    image_urls = [
+        image["src"]
+        for image in images
+        if not image["src"].endswith(".gif")
+        and "play-button-overlay" not in image["src"]
+    ]
+
+    currency = "EUR"
+    price_range = soup.find("span", {"class": "a-price-range"})
+    prices = price_range.find_all("span", {"class": "a-offscreen"})
+    price = float(prices[-1].text[1:])  # Return the maximum price
+
+    sustainability_spans = soup.find_all("span", id=re.compile("CPF-BTF-Certificate-Name"))
+    sustainability_texts = [span.text for span in sustainability_spans]
+    sustainability_labels = sustainability_label_to_certificate(sustainability_texts)
+
+    brand = _find_from_details_section(soup, "Manufacturer")
+    asin = _find_from_details_section(soup, "ASIN")
+    description = soup.find("div", {"id": "productDescription"}).p.get_text().strip()
+
+    return Product(
+        timestamp=parsed_page.scraped_page.timestamp,
+        url=parsed_page.scraped_page.url,
+        merchant=parsed_page.scraped_page.merchant,
+        category=parsed_page.scraped_page.category,
+        name=name,
+        description=description,
+        brand=brand,
+        sustainability_labels=sustainability_texts,
+        price=price,
+        currency=currency,
+        image_urls=image_urls,
+        color=color,
+        size=size,
+        gtin=None,
+        asin=asin,
+    )
 
 def sustainability_label_to_certificate(labels) -> list[CertificateType]:
     sustainability_labels = load_and_get_sustainability_labels()
@@ -46,54 +93,3 @@ def _find_from_details_section(soup, prop):
     elif product_details_table:
         parent = product_details_table.find("th", text=re.compile(f"\s+{prop}\s+")).parent
         return parent.find("td").text.strip().replace("\u200e", "")
-
-
-def extract_amazon(parsed_page: ParsedPage) -> Optional[Product]:
-    soup = parsed_page.beautiful_soup
-
-    name = soup.find("span", {"id": "productTitle"}).text.strip()
-    color = soup.find(
-        "span", {"id": "inline-twister-expanded-dimension-text-color_name"}).text.strip()
-
-    sizes = soup.find_all(
-        "span", {"class", "a-size-base swatch-title-text-display swatch-title-text"})[1:]
-    size = ", ".join([size.text.strip() for size in sizes])
-
-    images = soup.find("div", {"id": "altImages"}).find_all("img")
-    image_urls = [
-        image["src"]
-        for image in images
-        if not image["src"].endswith(".gif")
-        and "play-button-overlay" not in image["src"]
-    ]
-
-    currency = "EUR"
-    price_range = soup.find("span", {"class": "a-price-range"})
-    prices = price_range.find_all("span", {"class": "a-offscreen"})
-    price = float(prices[-1].text[1:])  # Return the maximum price
-
-    sustainability_spans = soup.find_all("span", id=re.compile("CPF-BTF-Certificate-Name"))
-    sustainability_texts = [span.text for span in sustainability_spans]
-    sustainability_labels = sustainability_label_to_certificate(sustainability_texts)
-
-    brand = _find_from_details_section(soup, "Manufacturer")
-    asin = _find_from_details_section(soup, "ASIN")
-    description = soup.find("div", {"id": "productDescription"}).p.get_text().strip()
-
-    return Product(
-        timestamp=parsed_page.scraped_page.timestamp,
-        url=parsed_page.scraped_page.url,
-        merchant=parsed_page.scraped_page.merchant,
-        category=parsed_page.scraped_page.category,
-        name=name,
-        description=description,
-        brand=brand,
-        sustainability_labels=sustainability_labels,
-        price=price,
-        currency=currency,
-        image_urls=image_urls,
-        color=color,
-        size=size,
-        gtin=None,
-        asin=asin,
-    )
