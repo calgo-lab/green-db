a3ca1deaba87f1b28b9f0c9968978fef839cd898
wkrl <kwxrl@yandex.com>
2022-05-07 01:10:55 +0200

    Cleanup strings

--- scraping/scraping/spiders/amazon.py
+++ scraping/scraping/spiders/amazon.py
@@ -20,17 +20,16 @@ class AmazonSpider(BaseSpider):
         """
         # Save HTML to database
         self._save_SERP(response)
-        urls = response.css('div.a-row.a-size-base.a-color-base a::attr(href)').getall()
-        prices = response.css(
-            'div.a-row.a-size-base.a-color-base span.a-price-whole::text').getall()
+        urls = response.css("div.a-row.a-size-base.a-color-base a::attr(href)").getall()
+        prices = response.css("div.a-row.a-size-base.a-color-base span.a-price-whole::text").getall()
         products = dict(zip(urls, prices))
 
         # Yield request for each product
         logger.info(f"Number of products to be scraped {len(products)}")
         for key, value in products.items():
-            if 'refinements=p_n_cpf_eligible' in key:
+            if "refinements=p_n_cpf_eligible" in key:
                 yield SplashRequest(
-                    url=f'https://www.amazon.de{key}',
+                    url=f"https://www.amazon.de{key}",
                     callback=self.parse_PRODUCT,
                     meta={"add_meta": value},
                     endpoint="execute",
