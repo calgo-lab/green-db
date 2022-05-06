1ae466f23241b0ee456011c6ad91f9d2eff54ba1
wkrl <kwxrl@yandex.com>
2022-05-06 23:23:18 +0200

    Update test description

--- extract/tests/amazon/printer_test.py
+++ extract/tests/amazon/printer_test.py
@@ -5,7 +5,7 @@ from extract import extract_product
 from ..utils import read_test_html
 
 
-def test_amazon_basic() -> None:
+def test_amazon_electronics() -> None:
     timestamp = "2022-04-28 19:00:00"
     url = "https://www.amazon.de/Kyocera-Klimaschutz-System-Ecosys-P3145dn-Laserdrucker/dp/B07TXRHFYS/ref=sr_1_23?qid=1651401368&refinements=p_n_cpf_eligible%3A22579885031&s=computers&sr=1-23&th=1%22"  # noqa
     merchant = "amazon"
@@ -32,7 +32,7 @@ def test_amazon_basic() -> None:
         merchant=merchant,
         category=category,
         name="Kyocera Klimaschutz-System Ecosys P3145dn Laserdrucker: Schwarz-Weiß, Duplex-Einheit, 45 Seiten pro Minute. Inkl. Mobile Print Funktion",
-        description="Geben Sie Ihr Modell ein, um sicherzustellen, dass dieser Artikel passt.. Geben Sie Ihr Modell ein, um sicherzustellen, dass dieser Artikel passt.. um sicherzustellen, dass dieser Artikel passt.. Zuverlässig: Der professionelle Ecosys Schwarz-Weiß Bürodrucker druckt bis zu 45 A4 Seiten/Minute und ist ideal für den Einsatz in Büroumgebungen wie (Home)Office, Praxis oder Kanzlei. Umweltfreundlich: Klimaschutz-System = klimafreundlich drucken und kopieren. Weitere Infos zur Kyocera Klimaschutz Initiative finden Sie unter dem Suchbegriff printgreen bei Kyocera in Ihrem Browser. Leistungsstark: Der Profi-Netzwerkdrucker mit doppelseitigem Druck, 600 Blatt Standard Papierkapazität, USB und Gigabit LAN druckt effizient und geräuscharm mit der Funktion Leiser Druck. Langlebigkeit: Der Laser-Drucker ist aus hochwertigen Materialien mit einer extrem hohen Lebensdauer gefertigt und ist speziell für den Einsatz in Geschäftsumgebungen geeignet und konzipiert. Sicherheit: Umfangreiche, integrierte Sicherheitsfunktionen für Unternehmensnetzwerke durch SSL, IPsec und Vertraulichen Druck. ›",
+        description="Geben Sie Ihr Modell ein, um sicherzustellen, dass dieser Artikel passt.. Zuverlässig: Der professionelle Ecosys Schwarz-Weiß Bürodrucker druckt bis zu 45 A4 Seiten/Minute und ist ideal für den Einsatz in Büroumgebungen wie (Home)Office, Praxis oder Kanzlei. Umweltfreundlich: Klimaschutz-System = klimafreundlich drucken und kopieren. Weitere Infos zur Kyocera Klimaschutz Initiative finden Sie unter dem Suchbegriff printgreen bei Kyocera in Ihrem Browser. Leistungsstark: Der Profi-Netzwerkdrucker mit doppelseitigem Druck, 600 Blatt Standard Papierkapazität, USB und Gigabit LAN druckt effizient und geräuscharm mit der Funktion Leiser Druck. Langlebigkeit: Der Laser-Drucker ist aus hochwertigen Materialien mit einer extrem hohen Lebensdauer gefertigt und ist speziell für den Einsatz in Geschäftsumgebungen geeignet und konzipiert. Sicherheit: Umfangreiche, integrierte Sicherheitsfunktionen für Unternehmensnetzwerke durch SSL, IPsec und Vertraulichen Druck",
         brand="Kyocera",
         sustainability_labels=["certificate:BLUE_ANGEL"],
         price=599.00,
