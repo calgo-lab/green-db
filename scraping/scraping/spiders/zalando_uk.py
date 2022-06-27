5c4c2fc261a94921741a270cbfdd994d81bd4c54
BigDatalex <alex.flick@gmx.de>
2022-06-27 19:58:39 +0200

    Fix naming.

--- scraping/scraping/spiders/zalando_uk.py
+++ scraping/scraping/spiders/zalando_uk.py
@@ -1,11 +1,11 @@
 from logging import getLogger
 
-from core.constants import TABLE_NAME_SCRAPING_ZALANDO_UK
+from core.constants import TABLE_NAME_SCRAPING_ZALANDO_GB
 from .zalando_de import ZalandoSpider
 
 logger = getLogger(__name__)
 
 
 class ZalandoUkSpider(ZalandoSpider):
-    name = TABLE_NAME_SCRAPING_ZALANDO_UK
+    name = TABLE_NAME_SCRAPING_ZALANDO_GB
     allowed_domains = ["www.zalando.co.uk"]
