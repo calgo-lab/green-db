b8cdc004a3a46eb09cc90cc82e886aaf02629a4c
BigDatalex <alex.flick@gmx.de>
2022-07-04 11:22:15 +0200

    Some linting.

--- scraping/scraping/dupefilter.py
+++ scraping/scraping/dupefilter.py
@@ -1,18 +1,20 @@
 """
 To handle Request meta info properly a custom DupeFilter must be set.
 """
-from typing import Union, Optional, Iterable
+from typing import Iterable, Optional, Union
 
-from scrapy_splash.utils import dict_hash
 from scrapy.http.request import Request as ScrapyHttpRequest
-from scrapy_splash.request import SplashRequest
 from scrapy_splash.dupefilter import SplashAwareDupeFilter, splash_request_fingerprint
+from scrapy_splash.request import SplashRequest
+from scrapy_splash.utils import dict_hash
 
 META_KEYS_FOR_FINGERPRINT = ["category", "gender", "consumer_lifestage"]
 
 
-def meta_request_fingerprint(request: Union[ScrapyHttpRequest, SplashRequest],
-                             include_headers: Optional[Iterable[Union[bytes, str]]] = None) -> str:
+def meta_request_fingerprint(
+    request: Union[ScrapyHttpRequest, SplashRequest],
+    include_headers: Optional[Iterable[Union[bytes, str]]] = None,
+) -> str:
     """
     Combines selected meta information with 'SplashAwareDupeFilter' generated fingerprint and
     returns new fingerprint.
@@ -32,5 +34,10 @@ class MetaAwareDupeFilter(SplashAwareDupeFilter):
     DupeFilter that includes selected meta information on top of 'SplashAwareDupeFilter'.
     It should be used with SplashMiddleware.
     """
+
     def request_fingerprint(self, request):
+        """
+        Return the request fingerprint. The request fingerprint is a hash that uniquely identifies
+        the resource the request points to.
+        """
         return meta_request_fingerprint(request)
