5af2577eb9cfc66e97d513fcc47b1dad1cfc3e7f
BigDatalex <alex.flick@gmx.de>
2022-07-06 14:22:40 +0200

    Minor and docu.

--- scraping/scraping/dupefilter.py
+++ scraping/scraping/dupefilter.py
@@ -11,33 +11,38 @@ from scrapy_splash.utils import dict_hash
 META_KEYS_FOR_FINGERPRINT = ["category", "gender", "consumer_lifestage"]
 
 
-def meta_request_fingerprint(
-    request: Union[ScrapyHttpRequest, SplashRequest],
-    include_headers: Optional[Iterable[Union[bytes, str]]] = None,
-) -> str:
-    """
-    Combines selected meta information with 'SplashAwareDupeFilter' generated fingerprint and
-    returns new fingerprint.
-
-    Args:
-    request Union[ScrapyHttpRequest, SplashRequest]: Request object on which to yield a request
-    include_headers Optional[Iterable[Union[bytes, str]]]: request headers to consider
-    """
-    splash_fp = splash_request_fingerprint(request, include_headers=include_headers)
-    meta = {k: v for k, v in request.meta.items() if k in META_KEYS_FOR_FINGERPRINT}
-    fp_dict = meta | {"splash_fp": splash_fp}
-    return dict_hash(fp_dict)
-
-
 class MetaAwareDupeFilter(SplashAwareDupeFilter):
     """
     DupeFilter that includes selected meta information on top of 'SplashAwareDupeFilter'.
     It should be used with SplashMiddleware.
+
+    Considering the meta information is important if products (with the same url) are assigned to
+    multiple product categories or genders. Scraping an url again with a different category/ gender
+    allows us to write another entry to the GreenDB with those meta information, which would
+    be normally filtered by 'SplashAwareDupeFilter'.
     """
+
+    @staticmethod
+    def meta_request_fingerprint(
+        request: Union[ScrapyHttpRequest, SplashRequest],
+        include_headers: Optional[Iterable[Union[bytes, str]]] = None,
+    ) -> str:
+        """
+        Combines selected meta information with 'SplashAwareDupeFilter' generated fingerprint and
+        returns new fingerprint.
+
+        Args:
+        request Union[ScrapyHttpRequest, SplashRequest]: Request object on which to yield a request
+        include_headers Optional[Iterable[Union[bytes, str]]]: request headers to consider
+        """
+        splash_fp = splash_request_fingerprint(request, include_headers=include_headers)
+        meta = {k: v for k, v in request.meta.items() if k in META_KEYS_FOR_FINGERPRINT}
+        fp_dict = meta | {"splash_fp": splash_fp}
+        return dict_hash(fp_dict)
 
     def request_fingerprint(self, request):
         """
         Return the request fingerprint. The request fingerprint is a hash that uniquely identifies
         the resource the request points to.
         """
-        return meta_request_fingerprint(request)
+        return self.meta_request_fingerprint(request)
