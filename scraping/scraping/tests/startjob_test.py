aa808f55dddf12ff2335ef1314d85a831322a4da
en-GB <67548055+en-GB@users.noreply.github.com>
2022-07-26 12:56:18 +0200

    fix test

--- scraping/scraping/tests/startjob_test.py
+++ scraping/scraping/tests/startjob_test.py
@@ -19,24 +19,32 @@ def test_startjob() -> None:
     for merchant, settings in SETTINGS.items():
         for setting in settings:
             assert "start_urls" in setting
+            assert "category" in setting
+
             start_urls = setting["start_urls"]
+            category = setting["category"]
+            gender = setting.get("gender")
+            consumer_lifestage = setting.get("consumer_lifestage")
+            meta_data = setting.get("meta_data")
+
             if isinstance(start_urls, list):
                 assert all([isinstance(start_url, str) for start_url in start_urls])
             else:
                 assert isinstance(start_urls, str)
-            assert "category" in setting
-            category = setting["category"]
+
             if isinstance(category, tuple):
                 assert len(category) == 2
                 category, category_meta_data = category
                 assert isinstance(category_meta_data, dict)
             assert enum_has_value(ProductCategory, category)
-            if "gender" in setting:
-                assert enum_has_value(GenderType, setting["gender"])
-            if "consumer_lifestage" in setting:
-                assert enum_has_value(ConsumerLifestageType, setting["consumer_lifestage"])
-            if "meta_data" in setting:
-                meta_data = setting["meta_data"]
+
+            if gender is not None:
+                assert enum_has_value(GenderType, gender)
+
+            if consumer_lifestage is not None:
+                assert enum_has_value(ConsumerLifestageType, consumer_lifestage)
+
+            if meta_data is not None:
                 if isinstance(meta_data, str):
                     meta_data = json.loads(meta_data)
                 assert isinstance(meta_data, dict)
