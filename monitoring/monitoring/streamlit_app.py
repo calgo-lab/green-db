eee6ed5594523eeb9e553781517c84d0b3a317a9
AdriaSG <adrianasg.15@gmail.com>
2022-08-28 18:56:22 +0200

    Clean streamlit application.

--- monitoring/monitoring/streamlit_app.py
+++ monitoring/monitoring/streamlit_app.py
@@ -1,175 +1,80 @@
-import numpy as np
-import pandas as pd
-import plotly.express as px
-import plotly.graph_objects as go
 import streamlit as st
-from core import log
-from core.constants import (
-    TABLE_NAME_SCRAPING_AMAZON_DE,
-    TABLE_NAME_SCRAPING_AMAZON_FR,
-    TABLE_NAME_SCRAPING_AMAZON_GB,
-    TABLE_NAME_SCRAPING_ASOS_FR,
-    TABLE_NAME_SCRAPING_HM_FR,
-    TABLE_NAME_SCRAPING_OTTO_DE,
-    TABLE_NAME_SCRAPING_ZALANDO_DE,
-    TABLE_NAME_SCRAPING_ZALANDO_FR,
-    TABLE_NAME_SCRAPING_ZALANDO_GB,
+
+from monitoring.utils import (
+    all_timestamps_agg,
+    all_timestamps_merchant,
+    dates,
+    last_extraction_summary,
+    last_scraping_summary,
+    products_by_category,
+    products_by_label,
+    products_unknown_label,
 )
-from database.connection import GreenDB, Scraping
-
-log.setup_logger(__name__)
-
-CONNECTION_FOR_TABLE = {
-    TABLE_NAME_SCRAPING_AMAZON_DE: Scraping(TABLE_NAME_SCRAPING_AMAZON_DE),
-    TABLE_NAME_SCRAPING_AMAZON_FR: Scraping(TABLE_NAME_SCRAPING_AMAZON_FR),
-    TABLE_NAME_SCRAPING_AMAZON_GB: Scraping(TABLE_NAME_SCRAPING_AMAZON_GB),
-    TABLE_NAME_SCRAPING_ASOS_FR: Scraping(TABLE_NAME_SCRAPING_ASOS_FR),
-    TABLE_NAME_SCRAPING_OTTO_DE: Scraping(TABLE_NAME_SCRAPING_OTTO_DE),
-    TABLE_NAME_SCRAPING_ZALANDO_DE: Scraping(TABLE_NAME_SCRAPING_ZALANDO_DE),
-    TABLE_NAME_SCRAPING_ZALANDO_FR: Scraping(TABLE_NAME_SCRAPING_ZALANDO_FR),
-    TABLE_NAME_SCRAPING_ZALANDO_GB: Scraping(TABLE_NAME_SCRAPING_ZALANDO_GB),
-    TABLE_NAME_SCRAPING_HM_FR: Scraping(TABLE_NAME_SCRAPING_HM_FR),
-}
-
-green_db = GreenDB()
 
 st.set_page_config(page_icon="♻️", page_title="GreenDB")
 
 
-def extraction_summary_to_df(query):
-    return pd.DataFrame(
-        query, columns=["country", "merchant", "timestamp", "products"]
-    ).sort_values(by="country")
-
-
-def last_scraping_summary_to_df():
-    return pd.DataFrame(
-        [
-            (CONNECTION_FOR_TABLE[table_name].get_last_job_summary()[0])
-            for table_name in CONNECTION_FOR_TABLE
-        ],
-        columns=["country", "merchant", "timestamp", "products"],
-    ).sort_values(by="country")
-
-
-def all_scraping_summary_to_df():
-    return pd.DataFrame(
-        [
-            (CONNECTION_FOR_TABLE[table_name].get_all_last_job()[0])
-            for table_name in CONNECTION_FOR_TABLE
-        ],
-        columns=["country", "merchant", "timestamp", "products"],
-    ).sort_values(by="country")
-
-
-def category_stack_bars(df):
-    fig = px.bar(df, x="category", y="products", color="merchant", text="products")
-    return fig
-
-
-def all_timestamps_merchant():
-    """
-    Gets all timestamps by merchant for green_bd and extraction databases.
-
-    Args:
-        timestamp (datetime): `timestamp` to check availability
-
-    Returns:
-        bool: Whether `timestamp` is available
-    """
-    extraction = extraction_summary_to_df(green_db.get_all_last_job())
-    extraction["type"] = "extraction"
-    scraping = all_scraping_summary_to_df()
-    scraping["type"] = "scraping"
-    all_timestamps = pd.concat([extraction, scraping], ignore_index=True)
-    all_timestamps["date"] = pd.to_datetime(all_timestamps["timestamp"]).dt.date
-    return all_timestamps
-
-
-def all_timestamps_merchant_chart():
-    df = all_timestamps_merchant()
-    fig = px.line(df, x="date", y="products", color="merchant", symbol="type")
-    return fig
-
-
-def all_timestamps_df():
-    all_timestamps_df = all_timestamps_merchant().pivot_table(
-        values="products", index=["date"], columns="type", aggfunc=np.sum, fill_value=0
-    )
-    return all_timestamps_df
-
-
-def all_timestamps_chart(df):
-    timestamps = df.index
-    all_timestamps_fig = go.Figure()
-    all_timestamps_fig.add_trace(
-        go.Bar(x=timestamps, y=df["extraction"], name="Extraction", marker_color="green")
-    )
-    all_timestamps_fig.add_trace(
-        go.Bar(x=timestamps, y=df["scraping"], name="Scraping", marker_color="goldenrod")
-    )
-    return all_timestamps_fig
-
-
-def labels_summary_chart():
-    df = green_db.products_by_label()
-    fig = px.line(df, x="date", y="count", color="label", text="count")
-    return fig
-
-
 def main():
+    """
+    This function renders streamlit application, uses dataframes and charts from 'utils'.
+    """
     st.title("GreenDB")
     st.header("A Product-by-Product Sustainability Database")
-    st.subheader("Green-DB Summary")
-    h1, h2 = st.columns(2)
-    h1.write("Latest timestamp of data extraction:")
-    h2.write(green_db.get_latest_timestamp().date())
+    st.subheader("DB Summary")
 
-    m1, m2, m3 = st.columns(3)
+    with st.sidebar:
+        st.title("Overview")
+        st.write("From last data extraction on:")
+        st.write(dates()[0])
+        st.metric(
+            label="Number extracted products",
+            value=last_extraction_summary()["products"].sum(),
+        )
+        st.metric(
+            label="Number scraped products",
+            value=last_scraping_summary()["products"].sum(),
+        )
+        st.metric(
+            label="Number of categories", value=len(products_by_category()[1].groupby("category"))
+        )
+        st.metric(
+            label="Number of merchants", value=len(last_extraction_summary().groupby("merchant"))
+        )
 
-    m1.metric(
-        label="Number extracted of products",
-        value=extraction_summary_to_df(green_db.get_last_job_summary())["products"].sum(),
-    )
-    m2.metric(
-        label="Number of categories", value=len(green_db.get_category_summary().groupby("category"))
-    )
-    m3.metric(
-        label="Number of merchants", value=len(green_db.get_category_summary().groupby("merchant"))
-    )
-
-    st.write("Latest job summary by merchant")
-    summary_ext, summary_scr = st.tabs(["📀Extraction", " 🤖Scraping"])
-    summary_ext.dataframe(extraction_summary_to_df(green_db.get_last_job_summary()))
-    summary_scr.dataframe(last_scraping_summary_to_df())
+    st.write("Latest data extraction overview by merchant:")
+    ext_df, scr_df = st.tabs(["📀Extraction", " 🤖Scraping"])
+    ext_df.dataframe(last_extraction_summary())
+    scr_df.dataframe(last_scraping_summary())
 
     st.write("Categories by merchant")
     cat_chart, cat_df = st.tabs(["📊Chart", "📓Data"])
-    cat_chart.plotly_chart(
-        category_stack_bars(green_db.get_category_summary()), use_container_width=True
-    )
-    cat_df.dataframe(
-        green_db.get_category_summary().pivot_table(
-            values="products", index=["category"], columns="merchant", fill_value=0
-        )
-    )
+    cat_chart.plotly_chart(products_by_category()[0])
+    cat_df.dataframe(products_by_category()[1])
 
+    st.subheader("All timestamps Summary")
     st.write("Scraping and Extraction all timestamps")
     alltimes_chart, alltimes_df = st.tabs(["📊Chart", "📓Data"])
-    alltimes_chart.plotly_chart(all_timestamps_chart(all_timestamps_df()), use_container_width=True)
-    alltimes_df.dataframe(all_timestamps_df())
+    alltimes_chart.plotly_chart(all_timestamps_agg()[0], use_container_width=True)
+    alltimes_df.dataframe(all_timestamps_agg()[1])
 
     st.write("Scraping and Extraction all timestamps by merchant")
-    alltimesmer_chart, alltimesmer_df = st.tabs(["📊Chart", "📓Data"])
-    alltimesmer_chart.plotly_chart(all_timestamps_merchant_chart())
-    alltimesmer_df.dataframe(all_timestamps_merchant())
+    chart, df_1, df_2 = st.tabs(["📊Chart", "📓Data by merchant", "📓Data by merchant and country"])
+    chart.plotly_chart(all_timestamps_merchant()[0])
+    df_1.dataframe(all_timestamps_merchant()[1])
+    df_2.dataframe(all_timestamps_merchant()[2])
 
     st.subheader("Sustainability labels overview")
-    sus1, sus2, sus3 = st.columns(3)
-    sus1.write("Sustainability label's last update:")
-    sus2.write(green_db.last_update_sustainability_labels())
-    st.write("Unknown vs Known Sustainability labels")
-    st.plotly_chart(labels_summary_chart(), use_container_width=True)
+    sl1, sl2 = st.columns(2)
+    sl1.write("Sustainability label's last update:")
+    sl2.write(dates()[1])
+    st.write("Products by sustainability label(s)")
+    st.dataframe(products_by_label()[2])
+    st.write("Unknown vs Known Sustainability labels for all timestamps")
+    st.plotly_chart(products_by_label()[0], use_container_width=True)
+    with st.expander("Display chart data"):
+        st.table(products_by_label()[1])
+    st.write("List of products with 'certificate:UNKNOWN'")
+    st.dataframe(products_unknown_label())
 
 
 main()
