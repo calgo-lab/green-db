3cd7ebfc784e2606b9a7327907b21d412831277c
AdriaSG <adrianasg.15@gmail.com>
2022-08-29 12:38:42 +0200

    Adding function notes.

--- monitoring/monitoring/utils.py
+++ monitoring/monitoring/utils.py
@@ -1,23 +1,38 @@
+from typing import Any
+
 import numpy as np
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
-
-from typing import Any
 from database.connection import GreenDB
+
 from monitoring import CONNECTION_FOR_TABLE
 
 green_db = GreenDB()
 
 
-def dates() -> Any:
+def dates() -> tuple:
+    """
+    Gets latest timestamp for green-db and sustainability-labels tables and converts it to date
+    format.
+
+    Returns:
+        List: containing date of last extraction from green-db and date of last update from
+        sustainability-labels
+    """
     last_extraction = green_db.get_latest_timestamp().date()
     last_label_update = green_db.last_update_sustainability_labels().date()
     return last_extraction, last_label_update
 
 
-# Scraping DB queries
 def all_scraping_summary() -> Any:
+    """
+    Uses 'get_scraping_summary' method to query all ScrapingDB tables.
+
+    Returns:
+        Dataframe: with number of products by country and merchant for all timestamps for all
+        existing tables in ScrapingDB
+    """
     all_scraping = []
     [all_scraping.extend(value.get_scraping_summary()) for value in CONNECTION_FOR_TABLE.values()]
     return pd.DataFrame(
@@ -26,25 +41,54 @@ def all_scraping_summary() -> Any:
 
 
 def last_scraping_summary() -> Any:
+    """
+    Filters returned dataframe from 'all_scraping_summary' by latest timestamp found in green-db
+    table.
+
+    Returns:
+        Dataframe: with number of products by country and merchant for latest timestamp for all
+        existing tables in ScrapingDB
+    """
     return all_scraping_summary()[
         (all_scraping_summary()["timestamp"] >= green_db.get_latest_timestamp())
     ]
 
 
-# Extraction DB queries
 def all_extraction_summary() -> Any:
+    """
+    Uses 'get_extraction_summary' output and converts it in a dataframe.
+
+    Returns:
+        Dataframe: with number of products by country and merchant for all timestamps in green-db
+        table.
+    """
     return pd.DataFrame(
         green_db.get_extraction_summary(), columns=["merchant", "timestamp", "products", "country"]
     ).sort_values(by="merchant")
 
 
 def last_extraction_summary() -> Any:
+    """
+    Filters returned dataframe from 'all_extraction_summary' by latest timestamp found in
+    green-db table.
+
+    Returns:
+        Dataframe: with number of products by country and merchant for latest timestamps in green-db
+        table.
+    """
     return all_extraction_summary()[
         (all_extraction_summary()["timestamp"] >= green_db.get_latest_timestamp())
     ]
 
 
-def products_by_category() -> Any:
+def products_by_category() -> tuple:
+    """
+    Gets 'get_category_summary' output, converts it in a dataframe and creates a bar chart for
+    products per category.
+
+    Returns:
+        List: [plotly chart, pandas dataframe]
+    """
     df = pd.DataFrame(green_db.get_category_summary(), columns=["category", "merchant", "products"])
     df_products_by_category = df.pivot_table(
         values="products", index=["category"], columns="merchant", fill_value=0
@@ -55,8 +99,14 @@ def products_by_category() -> Any:
     return fig_products_by_category, df_products_by_category
 
 
-# Both scraping and extraction
-def all_timestamps_merchant() -> Any:
+def all_timestamps_merchant() -> tuple:
+    """
+    Concatenates 'all_extraction_summary' and 'all_scraping_summary' dataframes and creates a
+    line chart for extracted and scraped products for all timestamps found by merchant and country.
+
+    Returns:
+        List: [plotly chart, pandas dataframe, pandas dataframe]
+    """
     extraction = all_extraction_summary()
     extraction["type"] = "extraction"
     scraping = all_scraping_summary()
@@ -86,7 +136,14 @@ def all_timestamps_merchant() -> Any:
     return fig_timestamps_by_merchant, df_timestamps_by_merchant, df_timestamps_by_merchant_country
 
 
-def all_timestamps_agg() -> Any:
+def all_timestamps_agg() -> tuple:
+    """
+    Creates an aggregated view of 'all_timestamps_merchant'. Resulting objects show total
+    number of extracted and scraped products for all timestamps found.
+
+    Returns:
+        List: [plotly chart, pandas dataframe]
+    """
     df_timestamps_agg = (
         all_timestamps_merchant()[2]
         .pivot_table(
@@ -109,12 +166,17 @@ def all_timestamps_agg() -> Any:
     return fig_timestamps_agg, df_timestamps_agg
 
 
-def products_by_label() -> Any:
+def products_by_label() -> tuple:
+    """
+    Gets 'products_by_sustainability_label' from green-db table, counts products with
+    "certificate:UNKNOWN" and products with any other label and creates a dataframe and a line
+    chart from it. It also filters given query to present products by sustainability label(s)
+    from last product extraction in a dataframe.
+
+    Returns:
+        List: [plotly chart, pandas dataframe, pandas dataframe]
+    """
     query = green_db.products_by_sustainability_label()
-    all = pd.DataFrame(query, columns=["timestamp", "labels", "count"])
-    last_extraction = all[(all["timestamp"] >= green_db.get_latest_timestamp())].sort_values(
-        "count", ascending=False
-    )
     unknown = []
     known = []
     for row in query:
@@ -134,20 +196,30 @@ def products_by_label() -> Any:
     known_cumm = known_df.groupby("date").sum()
     known_cumm["label"] = "Known certificates"
     join_df = pd.concat([unknown_cumm, known_cumm]).reset_index()
-    fig = px.line(join_df, x="date", y="count", color="label", text="count")
-    pivot = join_df.pivot_table(
+    fig_label_unknown = px.line(join_df, x="date", y="count", color="label", text="count")
+    df_label_unknown = join_df.pivot_table(
         values="count", index=["date"], columns="label", aggfunc=np.sum, fill_value=0
     ).sort_values(by="date", ascending=False)
-    return fig, pivot, last_extraction[["labels", "count"]]
+    all = pd.DataFrame(query, columns=["timestamp", "labels", "count"])
+    print(all)
+    last_extraction = all[(all["timestamp"] >= green_db.get_latest_timestamp())].sort_values(
+        "count", ascending=False
+    )
+    return fig_label_unknown, df_label_unknown, last_extraction[["labels", "count"]]
 
 
 def products_unknown_label() -> Any:
+    """
+    Gets 'products_by_sustainability_label_timestamp' for last timestamp found in green-db table
+    and filter products with "certificate:UNKNOWN".
+
+    Returns:
+        Dataframe: listing product id, name, merchant, url and sustainability label(s)
+    """
     query = green_db.products_by_sustainability_label_timestamp(green_db.get_latest_timestamp())
     unknown = []
     for row in query:
         for item in row:
             if type(item) is list:
                 [unknown.append(row) for label in item if label == "certificate:UNKNOWN"]
-    return pd.DataFrame(
-        unknown, columns=["id", "timestamp", "merchant", "name", "url", "labels"]
-    )
+    return pd.DataFrame(unknown, columns=["id", "timestamp", "merchant", "name", "url", "labels"])
