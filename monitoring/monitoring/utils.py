8808e2c7a5b6cbad79cc56bcb7096af1c8ab621c
AdriaSG <adrianasg.15@gmail.com>
2022-08-29 12:58:21 +0200

    Fix isort

--- monitoring/monitoring/utils.py
+++ monitoring/monitoring/utils.py
@@ -4,8 +4,8 @@ import numpy as np
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
-from database.connection import GreenDB
 
+from database.connection import GreenDB
 from monitoring import CONNECTION_FOR_TABLE
 
 green_db = GreenDB()
