cde6579ab0798f54456bc98d40ed092ec73c0a28
AdriaSG <adrianasg.15@gmail.com>
2022-09-02 18:40:49 +0200

    Refactoring app and utils.

--- monitoring/monitoring/monitoring_app.py
+++ monitoring/monitoring/monitoring_app.py
@@ -1,82 +1,107 @@
 import streamlit as st
 
+from database.connection import GreenDB
+from database.tables import SustainabilityLabelsTable
 from monitoring.utils import (
-    all_timestamps,
-    dates,
-    labels_known_vs_unknown,
-    latest_extraction_summary,
-    latest_scraping_summary,
-    products_by_category,
-    products_by_label,
+    get_all_timestamps_objects,
+    get_known_vs_unknown_certificates_chart,
+    get_latest_extraction_objects,
+    get_latest_scraping_objects,
+    get_products_by_category_objects,
 )
+
+green_db = GreenDB()
 
 
 def main() -> None:
     """
-    This function renders streamlit application, uses dataframes and charts from 'utils'.
+    This function renders streamlit application, uses methods from 'utils' to create a project
+    report. Holds connection to the database.
     """
+    # Header section with titles
     st.set_page_config(page_icon="♻️", page_title="GreenDB")
     st.title("GreenDB")
     st.header("A Product-by-Product Sustainability Database")
     st.subheader("DB Summary")
 
-    last_timestamps = dates()
-    latest_extraction_obj = latest_extraction_summary()
-    latest_scraping_obj = latest_scraping_summary()
-    cat_obj = products_by_category()
+    # Adds first session states
+    if "latest_extraction" not in st.session_state:
+        st.session_state.latest_extraction = get_latest_extraction_objects(
+            green_db.get_latest_extraction_summary()
+        )
+    if "latest_scraping" not in st.session_state:
+        st.session_state.latest_scraping = get_latest_scraping_objects()
+    if "extraction_by_category" not in st.session_state:
+        st.session_state.extraction_by_category = get_products_by_category_objects(
+            green_db.get_latest_category_summary()
+        )
 
+    # Sidebar with general overview of the project
     with st.sidebar:
         st.title("Overview")
         st.write("From last data extraction on:")
-        st.write(last_timestamps[0])
+        st.write(green_db.get_latest_timestamp().date())
         st.metric(
             label="Number extracted products",
-            value=latest_extraction_obj[1],
+            value=st.session_state.latest_extraction["total_extracted"],
         )
         st.metric(
             label="Number scraped products",
-            value=latest_scraping_obj[1],
+            value=st.session_state.latest_scraping["total_scraped"],
         )
-        st.metric(label="Number of categories", value=cat_obj[2])
-        st.metric(label="Number of merchants", value=latest_extraction_obj[2])
+        st.metric(
+            label="Number of categories",
+            value=st.session_state.extraction_by_category["number_categories"],
+        )
+        st.metric(
+            label="Number of merchants",
+            value=st.session_state.latest_extraction["number_merchants"],
+        )
+        st.write("Sustainability label's last update:")
+        st.write(green_db.get_latest_timestamp(SustainabilityLabelsTable).date())
 
+    # Tabs with latest summary of extracted and scraped products.
     st.write("Latest data extraction overview by merchant:")
-    ext_df, scr_df = st.tabs(["📀Extraction", " 🤖Scraping"])
-    ext_df.dataframe(latest_extraction_obj[0])
-    scr_df.dataframe(latest_scraping_obj[0])
+    tab1, tab2 = st.tabs(["📀Extraction", " 🤖Scraping"])
+    with tab1:
+        st.dataframe(st.session_state.latest_extraction["df"])
+    with tab2:
+        st.dataframe(st.session_state.latest_scraping["df"])
 
-    st.write("Categories by merchant")
-    cat_chart, cat_df = st.tabs(["📊Chart", "📓Data"])
-    cat_chart.plotly_chart(cat_obj[0])
-    cat_df.dataframe(cat_obj[1])
+    # Main container, contains all the plots
+    with st.container():
+        if "all_timestamps" not in st.session_state:
+            st.session_state.all_timestamps = get_all_timestamps_objects(
+                green_db.get_extraction_summary()
+            )
+        st.subheader("All timestamps Summary")
+        st.write("Scraping and Extraction all timestamps")
+        st.plotly_chart(
+            st.session_state.all_timestamps["chart_by_timestamp"], use_container_width=True
+        )
+        st.write("Scraping and Extraction all timestamps by merchant")
+        st.plotly_chart(
+            st.session_state.all_timestamps["chart_by_merchant"], use_container_width=True
+        )
+        if st.button("Show detailed dataframe by merchant and country for all timestamps."):
+            st.dataframe(st.session_state.all_timestamps["pivot_timestamps_by_merchant_country"])
+        st.write("Products by category and merchant")
+        st.plotly_chart(st.session_state.extraction_by_category["chart"])
+        st.write("Unknown vs Known certificates for all timestamps")
+        st.plotly_chart(
+            get_known_vs_unknown_certificates_chart(green_db.get_known_vs_unknown_certificates()),
+            use_container_width=True,
+        )
+        st.write("List of products with 'certificate:UNKNOWN'")
+        st.caption("From latest available timestamp")
+        if st.button("Show list of products"):
+            st.dataframe(green_db.get_latest_products_certificate_unknown())
 
-    st.subheader("All timestamps Summary")
-    all_timestamps_obj = all_timestamps()
-    st.write("Scraping and Extraction all timestamps")
-    alltimes_chart, alltimes_df = st.tabs(["📊Chart", "📓Data"])
-    alltimes_chart.plotly_chart(all_timestamps_obj[3], use_container_width=True)
-    alltimes_df.dataframe(all_timestamps_obj[4])
-
-    st.write("Scraping and Extraction all timestamps by merchant")
-    chart, df_1, df_2 = st.tabs(["📊Chart", "📓Data by merchant", "📓Data by merchant and country"])
-    chart.plotly_chart(all_timestamps_obj[0])
-    df_1.dataframe(all_timestamps_obj[1])
-    df_2.dataframe(all_timestamps_obj[2])
-
-    st.subheader("Sustainability labels overview")
-    sl1, sl2 = st.columns(2)
-    sl1.write("Sustainability label's last update:")
-    sl2.write(last_timestamps[1])
-    st.write("Products by sustainability label(s)")
-    label_df = products_by_label()
-    st.dataframe(label_df)
-    st.write("Unknown vs Known Sustainability labels for all timestamps")
-    unknown_certificates_obj = labels_known_vs_unknown()
-    st.plotly_chart(unknown_certificates_obj[0], use_container_width=True)
-    with st.expander("Display chart data"):
-        st.table(unknown_certificates_obj[1])
-    st.write("List of products with 'certificate:UNKNOWN'")
-    st.dataframe(unknown_certificates_obj[2])
+    # Last container to show dataframe of products by label
+    with st.container():
+        st.write("Products by sustainability label(s)")
+        if st.button("Show list of labels"):
+            st.dataframe(green_db.get_latest_products_by_label())
 
 
 main()
--- monitoring/monitoring/utils.py
+++ monitoring/monitoring/utils.py
@@ -2,118 +2,90 @@ import numpy as np
 import pandas as pd
 import plotly.express as px
 
-from database.connection import GreenDB
-from database.tables import SustainabilityLabelsTable
-
 from . import CONNECTION_FOR_TABLE
 
-green_db = GreenDB()
 
-
-def dates() -> tuple:
+def get_latest_extraction_objects(df: pd.DataFrame) -> dict:
     """
-    Gets latest timestamp for green-db and sustainability-labels tables and converts it to date
-    format.
+    Fetch number of extracted products for latest available timestamp by merchant. Counts number
+    of merchants.
 
     Returns:
-        Tuple: with date of last extraction from green-db and date of last update from
-        sustainability-labels
+        Dict: with objects to display in the monitoring app.
     """
-    last_extraction = green_db.get_latest_timestamp().date()
-    last_label_update = green_db.get_latest_timestamp(SustainabilityLabelsTable).date()
-    return last_extraction, last_label_update
+    return {
+        "df": df,
+        "total_extracted": df["product_count"].sum(),
+        "number_merchants": len(df.groupby("merchant")),
+    }
 
 
-def latest_extraction_summary() -> tuple:
+def get_latest_scraping_objects() -> dict:
     """
-    Uses 'get_latest_extraction_summary' to fetch number of extracted products for latest available
-    timestamp by merchant. Counts number of merchants.
+    Uses 'get_latest_scraping_summary' to fetch number of scraped products for latest
+    available timestamp by merchant, query all tables in ScrapingDB and concatenates the result in a
+    single dataframe.
 
     Returns:
-        Tuple: with objects to display in the monitoring app.
-    """
-    df = green_db.get_latest_extraction_summary()
-    number_products = df["products"].sum()
-    number_merchants = len(df.groupby("merchant"))
-    return df, number_products, number_merchants
-
-
-def latest_scraping_summary() -> tuple:
-    """
-    Uses 'get_latest_scraping_summary' to fetch number of scraped products for latest available
-    timestamp by merchant, query all tables in ScrapingDB and concatenates the result in a
-    DataFrame.
-
-    Returns:
-        Tuple: with objects to display in the monitoring app.
+        Dict: with objects to display in the monitoring app.
     """
     all_scraping = []
     for value in CONNECTION_FOR_TABLE.values():
-        all_scraping.extend(value.get_latest_scraping_summary())
-    df = pd.DataFrame(
-        all_scraping, columns=["merchant", "timestamp", "products", "country"]
-    ).sort_values(by="merchant")
-    number_products = df["products"].sum()
-    return df, number_products
+        all_scraping.append(value.get_latest_scraping_summary())
+    df = pd.concat(all_scraping)
+    return {"df": df, "total_scraped": df["product_count"].sum()}
 
 
-def products_by_category() -> tuple:
+def get_products_by_category_objects(query: pd.DataFrame) -> dict:
     """
     Uses 'get_latest_category_summary' output to create a bar chart for products per category
     and a pivot table. Counts number of categories.
 
-    Returns:
-        Tuple: with objects to display in the monitoring app.
-    """
-    df = green_db.get_latest_category_summary()
-    df_products_by_category = df.pivot_table(
-        values="products", index=["category"], columns="merchant", fill_value=0
-    )
-    fig_products_by_category = px.bar(
-        df, x="category", y="products", color="merchant", text="products"
-    )
-    number_cat = len(df.groupby("category"))
-    return fig_products_by_category, df_products_by_category, number_cat
-
-
-def all_timestamps() -> tuple:
-    """
-    Concatenates 'all_extraction_summary' and 'all_scraping_summary' Dataframes to create:
-    1) Line chart and DataFrame for extracted and scraped products for all timestamps found by
-    merchant. A second DataFrame including country level.
-    2) Line chart and DataFrame for extracted and scraped products for all timestamps
+    Args:
+        query (pd.DataFrame): Data to plot.
 
     Returns:
-        Tuple: with objects to display in the monitoring app.
+        Dict: with objects to display in the monitoring app.
     """
-    extraction = green_db.get_extraction_summary()
+    return {
+        "chart": px.bar(
+            query, x="category", y="product_count", color="merchant", text="product_count"
+        ),
+        "number_categories": len(query.groupby("category")),
+    }
+
+
+def get_all_timestamps_objects(extraction: pd.DataFrame) -> dict:
+    """
+    Concatenates 'all_extraction_summary' and 'all_scraping_summary' dataframes to create:
+    1) Line chart for extracted and scraped products for all timestamps found by
+    merchant.
+    2) Line chart for extracted and scraped products for all timestamps.
+    3) Join DataFrame.
+
+    Args:
+        extraction (pd.DataFrame): Data to plot.
+
+    Returns:
+        Dict: with objects to display in the monitoring app.
+    """
     extraction["type"] = "extraction"
     all_scraping = []
     for value in CONNECTION_FOR_TABLE.values():
-        all_scraping.extend(value.get_scraping_summary())
-    scraping = pd.DataFrame(
-        all_scraping, columns=["merchant", "timestamp", "products", "country"]
-    ).sort_values(by="merchant")
+        all_scraping.append(value.get_scraping_summary())
+    scraping = pd.concat(all_scraping)
     scraping["type"] = "scraping"
-    all_timestamps = pd.concat([extraction, scraping], ignore_index=True)
-    all_timestamps["date"] = pd.to_datetime(all_timestamps["timestamp"]).dt.date
-    df_timestamps_by_merchant = (
-        all_timestamps.groupby(by=["date", "merchant", "type"])
-        .sum()
-        .pivot_table(
-            values="products",
-            index=["date", "merchant"],
-            columns="type",
-            aggfunc=np.sum,
-            fill_value=0,
-        )
-        .sort_values(by="date", ascending=False)
+    all_timestamps_concat = pd.concat([extraction, scraping], ignore_index=True)
+    all_timestamps_concat["date"] = pd.to_datetime(all_timestamps_concat["timestamp"]).dt.date
+    df_agg_merchant = (
+        all_timestamps_concat.groupby(by=["date", "merchant", "type"]).sum().reset_index()
     )
-    df_timestamps_by_merchant_country = (
-        all_timestamps.groupby(by=["date", "merchant", "country", "type"])
+    df_agg_timestamp = all_timestamps_concat.groupby(by=["date", "type"]).sum().reset_index()
+    pivot = (
+        all_timestamps_concat.groupby(by=["date", "merchant", "country", "type"])
         .sum()
         .pivot_table(
-            values="products",
+            values="product_count",
             index=["date", "merchant", "country"],
             columns="type",
             aggfunc=np.sum,
@@ -121,45 +93,25 @@ def all_timestamps() -> tuple:
         )
         .sort_values(by="date", ascending=False)
     )
-    df = all_timestamps.groupby(by=["date", "merchant", "type"]).sum().reset_index()
-    fig_timestamps_by_merchant = px.line(
-        df, x="date", y="products", color="merchant", symbol="type"
-    )
-    df_agg = all_timestamps.groupby(by=["date", "type"]).sum().reset_index()
-    df_timestamps_agg = df_agg.pivot_table(
-        values="products", index=["date"], columns="type", aggfunc=np.sum, fill_value=0
-    ).sort_values(by="date", ascending=False)
-    fig_timestamps_agg = px.line(df_agg, x="date", y="products", color="type")
-    return (
-        fig_timestamps_by_merchant,
-        df_timestamps_by_merchant,
-        df_timestamps_by_merchant_country,
-        fig_timestamps_agg,
-        df_timestamps_agg,
-    )
+    return {
+        "chart_by_merchant": px.line(
+            df_agg_merchant, x="date", y="product_count", color="merchant", symbol="type"
+        ),
+        "chart_by_timestamp": px.line(df_agg_timestamp, x="date", y="product_count", color="type"),
+        "pivot_timestamps_by_merchant_country": pivot,
+    }
 
 
-def products_by_label() -> pd.DataFrame:
+def get_known_vs_unknown_certificates_chart(query: pd.DataFrame) -> px.line:
     """
-    Returns:
-        DataFrame: contains products by sustainability label for latest available timestamp.
-    """
-    return green_db.get_latest_products_by_label()
+    Creates a line chart to compare number of products with 'certificate:UNKNOWN' against product
+    with "Known certificates".
 
-
-def labels_known_vs_unknown() -> tuple:
-    """
-    Gets 'get_labels_known_vs_unknown' from green-db table and creates a line chart to compare
-    number of products with 'certificate:UNKNOWN' against product with "Known certificates".
+    Args:
+        query (pd.DataFrame): Data to plot.
 
     Returns:
-        Tuple: with objects to display in the monitoring app.
+        Chart: line chart to display in the monitoring app.
     """
-    query = green_db.get_labels_known_vs_unknown()
     query["date"] = pd.to_datetime(query["timestamp"]).dt.date
-    fig_label_unknown = px.line(query, x="date", y="count", color="label", symbol="label")
-    fig_label_unknown.show()
-    df_label_unknown = query.pivot_table(
-        values="count", index=["date"], columns="label", aggfunc=np.sum, fill_value=0
-    ).sort_values(by="date", ascending=False)
-    return (fig_label_unknown, df_label_unknown, green_db.get_latest_products_certificate_unknown())
+    return px.line(query, x="date", y="count", color="label", symbol="label")
