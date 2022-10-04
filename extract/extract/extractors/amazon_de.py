a440f235ebe9627eac4976504ad43ba14c4fb79f
BigDatalex <alex.flick@gmx.de>
2022-10-04 23:40:23 +0200

    Linting.

--- extract/extract/extractors/amazon_de.py
+++ extract/extract/extractors/amazon_de.py
@@ -178,7 +178,6 @@ def get_repairability_index(soup: BeautifulSoup) -> Optional[str]:
             repairability_index = float(repairability_index.find("img").get("alt"))
             if repairability_index < 8:
                 repairability_index = int(repairability_index // 2) * 2
-                #French Repair Index: 0 - 1.9
                 return f"French Repair Index: {repairability_index} - {repairability_index+1.9}"
             else:
                 return "French Repair Index: 8 - 10"
