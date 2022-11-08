ea87b8f4a7a09cde037798791a5cb18a4edb4a5c
Sebastian Jäger <git@sebastian-jaeger.me>
2022-11-08 15:05:19 +0100

    more formatting

--- scraping/scraping/middlewares.py
+++ scraping/scraping/middlewares.py
@@ -28,12 +28,16 @@ class AmazonSchedulerMiddleware(object):
     def process_request(self, request: Any, spider: Any) -> Any:
         self.request_counter += 1
         current_time = monotonic()
+
         if self.request_counter >= 250:
             self.end_of_break_time = current_time + 60**2 * 12 * (random() + 0.5)
             self.request_counter = 0
+
         if current_time < self.end_of_break_time:
             delay = self.end_of_break_time - current_time
             logger.info(f"Delaying {request} by {delay} seconds")
-            d: Any = Deferred()
+            d: Deferred = Deferred()
+
             reactor.callLater(delay, d.callback, None)  # type: ignore[attr-defined]
+
             return d
