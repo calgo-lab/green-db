a1207cb74eedac5b05191cae292a240278ab9675
Alex Flick <alex.flick@gmx.de>
2023-03-06 13:32:44 +0100

    Combine all zalando label mappings from all countries.

--- extract/extract/extractors/zalando_fr.py
+++ extract/extract/extractors/zalando_fr.py
@@ -9,52 +9,5 @@ from .zalando_de import extract_zalando_de
 logger = getLogger(__name__)
 
 
-_LABEL_MAPPING = {
-    "": CertificateType.OTHER,
-    "Biodégradable": CertificateType.OTHER,
-    "Biologique": CertificateType.OTHER,
-    "bluesign® APPROVED materials": CertificateType.BLUESIGN_APPROVED,
-    "Bronze Cradle to Cradle Certified®": CertificateType.CRADLE_TO_CRADLE_BRONZE,
-    "Argent Cradle to Cradle Certified® ": CertificateType.CRADLE_TO_CRADLE_SILVER,
-    "Or Cradle to Cradle Certified® ": CertificateType.CRADLE_TO_CRADLE_GOLD,
-    "Fabriqué avec 30-50% de matériaux recyclés": CertificateType.OTHER,
-    "Fabriqué avec 50-70% de matériaux organiques": CertificateType.OTHER,
-    "Fabriqué avec 50-70% de matériaux recyclés": CertificateType.OTHER,
-    "Fabriqué avec 70-100% de matériaux organiques": CertificateType.OTHER,
-    "Fabriqué avec 70-100% de matériaux recyclés": CertificateType.OTHER,
-    "Fabriqué avec au moins 20 % d'alternatives biologiques innovantes aux combustibles fossiles": CertificateType.OTHER,  # noqa
-    "Fabriqué avec au moins 20% d'alternatives innovantes au cuir": CertificateType.OTHER,
-    "Fabriqué avec au moins 20% de coton recyclé": CertificateType.OTHER,
-    "Fabriqué avec au moins 20% de matériaux innovants recyclés à partir de déchets": CertificateType.OTHER,  # noqa
-    "Fabriqué avec au moins 50 % de polyuréthane à base d'eau": CertificateType.OTHER,
-    "Fabriqué avec au moins 50% de coton provenant de sources durables.": CertificateType.OTHER,
-    "Fabriqué avec au moins 50% de lyocell": CertificateType.OTHER,
-    "Fabriqué avec au moins 50% de matériaux responsables issus de forêts": CertificateType.OTHER,
-    "Fabriqué avec de la laine provenant de sources responsables": CertificateType.OTHER,
-    "Fabriqué avec du duvet provenant de sources responsables": CertificateType.OTHER,
-    "Water-Based Polyurethane": CertificateType.OTHER,
-    "Fairtrade Certified Cotton": CertificateType.FAIRTRADE_COTTON,
-    "Fairtrade Cotton": CertificateType.FAIRTRADE_COTTON,
-    "Global Recycled Standard": CertificateType.GLOBAL_RECYCLED_STANDARD,
-    "Global Recycled Standard (GRS)": CertificateType.GLOBAL_RECYCLED_STANDARD,
-    "GOTS - made with organic materials": CertificateType.GOTS_MADE_WITH_ORGANIC_MATERIALS,
-    "Global Organic Textile Standard (GOTS) - Made with Organic": CertificateType.GOTS_MADE_WITH_ORGANIC_MATERIALS,  # noqa
-    "Global Organic Textile Standard (GOTS) - Organic": CertificateType.GOTS_ORGANIC,
-    "GOTS - organic": CertificateType.GOTS_ORGANIC,
-    "Moins d’emballage": CertificateType.OTHER,
-    "Naturel": CertificateType.OTHER,
-    "OCS - Organic Blended Content Standard": CertificateType.ORGANIC_CONTENT_STANDARD_BLENDED,
-    "Organic Content Standard (OCS) - Blended": CertificateType.ORGANIC_CONTENT_STANDARD_BLENDED,
-    "Organic Content Standard (OCS) - 100": CertificateType.ORGANIC_CONTENT_STANDARD_100,
-    "OCS - Organic Content Standard": CertificateType.ORGANIC_CONTENT_STANDARD_100,
-    "Respectueux des animaux": CertificateType.OTHER,
-    "Respectueux des forêts": CertificateType.OTHER,
-    "Responsible Wool Standard": CertificateType.RESPONSIBLE_WOOL_STANDARD,
-    "Lyocell": CertificateType.OTHER,
-}
-
-
-def extract_zalando_fr(
-    parsed_page: ParsedPage, label_mapping: Dict[str, CertificateType] = _LABEL_MAPPING
-) -> Optional[Product]:
-    return extract_zalando_de(parsed_page=parsed_page, label_mapping=label_mapping)
+def extract_zalando_fr(parsed_page: ParsedPage) -> Optional[Product]:
+    return extract_zalando_de(parsed_page=parsed_page)
--- extract/extract/extractors/zalando_gb.py
+++ extract/extract/extractors/zalando_gb.py
@@ -9,46 +9,5 @@ from .zalando_de import extract_zalando_de
 logger = getLogger(__name__)
 
 
-_LABEL_MAPPING = {
-    "": CertificateType.OTHER,
-    "bluesign® APPROVED materials": CertificateType.BLUESIGN_APPROVED,
-    "Cradle to Cradle Certified ® Gold": CertificateType.CRADLE_TO_CRADLE_GOLD,
-    "Cradle to Cradle Certified ® Silver": CertificateType.CRADLE_TO_CRADLE_SILVER,
-    "Fairtrade Certified Cotton": CertificateType.FAIRTRADE_COTTON,
-    "Fairtrade Cotton": CertificateType.FAIRTRADE_COTTON,
-    "Global Recycle Standard": CertificateType.GLOBAL_RECYCLED_STANDARD,
-    "Global Recycled Standard (GRS)": CertificateType.GLOBAL_RECYCLED_STANDARD,
-    "GOTS - made with organic materials": CertificateType.GOTS_MADE_WITH_ORGANIC_MATERIALS,
-    "Global Organic Textile Standard (GOTS) - Organic": CertificateType.GOTS_ORGANIC,
-    "Global Organic Textile Standard (GOTS) - Made with Organic": CertificateType.GOTS_MADE_WITH_ORGANIC_MATERIALS,  # noqa
-    "GOTS - organic": CertificateType.GOTS_ORGANIC,
-    "Made with 30-50% recycled materials": CertificateType.OTHER,
-    "Made with 50-70% organic materials": CertificateType.OTHER,
-    "Made with 50-70% recycled materials": CertificateType.OTHER,
-    "Made with 70-100% organic materials": CertificateType.OTHER,
-    "Made with 70-100% recycled materials": CertificateType.OTHER,
-    "Made with at least 20% innovative bio-based alternatives to fossil fuels": CertificateType.OTHER,  # noqa
-    "Made with at least 20% innovative leather alternatives": CertificateType.OTHER,
-    "Made with at least 20% innovative materials upcycled from waste": CertificateType.OTHER,
-    "Made with at least 20% recycled cotton": CertificateType.OTHER,
-    "Made with at least 50% lyocell": CertificateType.OTHER,
-    "Made with at least 50% responsible forest-based materials": CertificateType.OTHER,
-    "Made with at least 50% sustainably-sourced cotton": CertificateType.OTHER,
-    "Made with at least 50% water-based polyurethane": CertificateType.OTHER,
-    "Made with responsibly-sourced down": CertificateType.OTHER,
-    "Made with responsibly-sourced wool": CertificateType.OTHER,
-    "OCS - Organic Blended Content Standard": CertificateType.ORGANIC_CONTENT_STANDARD_BLENDED,
-    "Organic Content Standard (OCS) - Blended": CertificateType.ORGANIC_CONTENT_STANDARD_BLENDED,
-    "OCS - Organic Content Standard": CertificateType.ORGANIC_CONTENT_STANDARD_100,
-    "Organic Content Standard (OCS) - 100": CertificateType.ORGANIC_CONTENT_STANDARD_100,
-    "Organic": CertificateType.OTHER,
-    "Responsible Wool Standard": CertificateType.RESPONSIBLE_WOOL_STANDARD,
-    "Lyocell": CertificateType.OTHER,
-    "Water-Based Polyurethane": CertificateType.OTHER,
-}
-
-
-def extract_zalando_uk(
-    parsed_page: ParsedPage, label_mapping: Dict[str, CertificateType] = _LABEL_MAPPING
-) -> Optional[Product]:
-    return extract_zalando_de(parsed_page=parsed_page, label_mapping=label_mapping)
+def extract_zalando_uk(parsed_page: ParsedPage) -> Optional[Product]:
+    return extract_zalando_de(parsed_page=parsed_page)
