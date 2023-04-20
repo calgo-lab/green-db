d63eb0eea5e76d95ce4f6ad0dcb173a058e2d0f6
Alex Flick <alex.flick@gmx.de>
2023-04-20 13:21:38 +0200

    Use new model with all categories.

--- product-classification/product_classification/app.py
+++ product-classification/product_classification/app.py
@@ -8,9 +8,7 @@ import numpy as np
 import pandas as pd
 from autogluon.multimodal import MultiModalPredictor
 from sklearn.preprocessing import LabelEncoder
-from datetime import datetime
 from core.constants import PRODUCT_CLASSIFICATION_MODEL, PRODUCT_CLASSIFICATION_MODEL_FEATURES
-from core.domain import ProductClassification
 
 from database.connection import GreenDB
 
@@ -18,50 +16,13 @@ logger = getLogger(__name__)
 
 app = Flask(__name__)
 
-model_classes = ['AUDIO HEADSETS', 'BLAZER', 'BLOUSE', 'CLOTHES WASHERS',
-                 'COOKER HOODS', 'DENIM_TROUSERS', 'DISHWASHERS', 'DRESSES',
-                 'FOOTWEAR', 'FREEZERS', 'JACKETS/CARDIGANS/WAISTCOATS',
-                 'OVENS', 'OVERALLS/BODYSUITS', 'PERSONAL CARRIERS',
-                 'PERSONAL COMPUTERS - PORTABLE', 'PRINTERS',
-                 'RANGE COOKERS/STOVES', 'REFRIGERATORS', 'RUCKSACK',
-                 'SHIRTS/POLO SHIRTS/T-SHIRTS', 'SKIRTS', 'SLEEPWEAR',
-                 'SLEEVELESS_SHIRT', 'SPIN/TUMBLE DRYERS', 'SWEATERS/PULLOVERS',
-                 'SWIMWEAR', 'TABLET PC', 'TRACKSUIT', 'TROUSERS/SHORTS', 'UNDERWEAR']
-
-gpc_to_greendb = {'AUDIO HEADSETS': 'HEADPHONES',
-                  'BLAZER': 'JACKET',
-                  'BLOUSE': 'BLOUSE',
-                  'CLOTHES WASHERS': 'WASHER',
-                  'COOKER HOODS': 'COOKER_HOOD',
-                  'DENIM_TROUSERS': 'JEANS',
-                  'DISHWASHERS': 'DISHWASHER',
-                  'DRESSES': 'DRESS',
-                  'FOOTWEAR': 'SHOES',
-                  'FREEZERS': 'FREEZER',
-                  'JACKETS/CARDIGANS/WAISTCOATS': 'JACKET',
-                  'OVENS': 'OVEN',
-                  'OVERALLS/BODYSUITS': 'OVERALL',
-                  'PERSONAL CARRIERS': 'BAG',
-                  'PERSONAL COMPUTERS - PORTABLE': 'LAPTOP',
-                  'PRINTERS': 'PRINTER',
-                  'RANGE COOKERS/STOVES': 'STOVE',
-                  'REFRIGERATORS': 'FRIDGE',
-                  'RUCKSACK': 'BACKPACK',
-                  'SHIRTS/POLO SHIRTS/T-SHIRTS': 'SHIRT',
-                  'SKIRTS': 'SKIRT',
-                  'SLEEPWEAR': 'NIGHTWEAR',
-                  'SLEEVELESS_SHIRT': 'TOP',
-                  'SPIN/TUMBLE DRYERS': 'DRYER',
-                  'SWEATERS/PULLOVERS': 'SWEATER',
-                  'SWIMWEAR': 'SWIMWEAR',
-                  'TABLET PC': 'TABLET',
-                  'TRACKSUIT': 'TRACKSUIT',
-                  'TROUSERS/SHORTS': 'PANTS',
-                  'UNDERWEAR': 'UNDERWEAR'}
-
-combine_classes = {
-    'JACKETS/CARDIGANS/WAISTCOATS': ['BLAZER', 'JACKETS/CARDIGANS/WAISTCOATS']
-}
+model_classes = ['BACKPACK', 'BAG', 'BLOUSE', 'COOKER_HOOD', 'DISHWASHER', 'DRESS',
+                 'DRYER', 'FREEZER', 'FRIDGE', 'HEADPHONES', 'HEADSET', 'JACKET',
+                 'JEANS', 'LAPTOP', 'LINEN', 'NIGHTWEAR', 'OVEN', 'OVERALL',
+                 'PANTS', 'PRINTER', 'SHIRT', 'SHOES', 'SHORTS', 'SKIRT',
+                 'SMARTPHONE', 'SMARTWATCH', 'SNEAKERS', 'SOCKS', 'STOVE', 'SUIT',
+                 'SWEATER', 'SWIMWEAR', 'TABLET', 'TOP', 'TOWEL', 'TRACKSUIT',
+                 'TSHIRT', 'TV', 'UNDERWEAR', 'WASHER']
 
 le = LabelEncoder()
 le.fit(model_classes)
@@ -71,6 +32,7 @@ db_connection = GreenDB()
 model = MultiModalPredictor.load(MODEL_DIR)
 model.set_num_gpus(0)
 
+
 def predict_proba(df):
     logger.info(f"Predicting for {len(df)} products ...")
     pred_probs = model.predict_proba(df)
@@ -79,10 +41,7 @@ def predict_proba(df):
 
 
 def create_ProductClassification(pred_probs) -> pd.DataFrame:
-    pred_probs = convert_classes(pred_probs)
     pred_probs.columns = le.inverse_transform(pred_probs.columns)
-    pred_probs.columns = [gpc_to_greendb.get(col) for col in pred_probs.columns]
-
     predicted_category = [pred_probs.columns[np.argmax(p)] for p in pred_probs.values]
     confidence = [np.max(p) for p in pred_probs.values]
 
@@ -96,56 +55,6 @@ def create_ProductClassification(pred_probs) -> pd.DataFrame:
 
     return result
 
-
-def convert_classes(pred_probs):
-    # combine all column values that have the same greendb category e.g. BLAZER and JACKET/CARDIGANS/WAISTCOATS
-
-    for k, v in combine_classes.items():
-        cols_to_combine = le.transform(v)
-        max_of_cols = pred_probs.loc[:, cols_to_combine].max(axis=1)
-        pred_probs[le.transform([k])[0]] = max_of_cols
-
-        drop_cols = [c for c in v if c != k]
-        pred_probs = pred_probs.drop(columns=le.transform(drop_cols))
-
-    return pred_probs
-
-
-def to_df(objects: Union[list, Iterator]) -> pd.DataFrame:
-    """Converts a list or iterator to a df.
-    :param objects: A list of objects to be converted to a df.
-    :return:
-        A dataframe from the `objects`.
-    """
-    return pd.DataFrame([obj.__dict__ for obj in objects])
-
-
-def get_latest_products():
-    # Get the products from the db
-    logger.info(f"Loading latest products from database ...")
-    products = db_connection.get_latest_products(convert_orm=False)
-    logger.info(f"Loaded {len(products)} latest products from database.")
-    products = to_df(products)
-    products = products.set_index("id")[PRODUCT_CLASSIFICATION_MODEL_FEATURES]
-    return products
-
-
-@app.route("/predict_latest")
-def certificates_persistence_handler() -> Response:
-    """Calls the persist_certificates() in order to precompute and persist the certificates.
-    :return:
-        A flask Response containing the precomputed certificates.
-    """
-    df = get_latest_products()
-
-    # TODO: predict only when results are not stored in db
-    pred_probs = predict_proba(df)
-    result = create_ProductClassification(pred_probs)
-
-    # TODO: write results to db
-    db_connection.write_dataframe(result)
-
-    return Response(result.to_json(orient="records"), mimetype="application/json")
 
 def eval_request(request_data):
     df = pd.read_json(request_data)
@@ -175,14 +84,6 @@ def product_classifier_handler() -> Response:
 
     result = create_ProductClassification(pred_probs)
     return Response(result.to_json(orient="records"), mimetype="application/json")
-
-
-# TODO: add label qualities via cleanlab as well (needs mapping from GreenDB to GPC)
-
-
-@app.route("/test", methods=['GET'])
-def test() -> Response:
-    return Response("test")
 
 
 def create_app():
